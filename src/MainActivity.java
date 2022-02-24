b5b8757f9868cdd24b7123f98fbb9b4ae72a440b
raghulmaniam@gmail.com <raghulmaniam@gmail.com>
2022-02-24 13:42:14 +0000

    initial commit comment

---
+++ src/MainActivity.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class MainActivity {
+
+	private StringBuilder output = new StringBuilder();
+	private final String inputstring;
+
+	public MainActivity(final String str) {
+		inputstring = str;
+		System.out.println("The input string  is  : " + inputstring);
+	}
+
+	public static void main(String args[]) {
+
+		long start2 = System.currentTimeMillis();
+
+		String comb = null;
+		System.out.println("Enter the string to find all the combinations:");
+		Scanner sc = new Scanner(System.in);
+		comb = sc.next();
+
+		MainActivity combobj = new MainActivity(comb);
+		System.out.println("");
+		System.out.println("");
+		System.out.println("All possible combinations are :  ");
+		System.out.println("");
+		System.out.println("");
+		combobj.combine();
+
+		long end2 = System.currentTimeMillis();
+
+		System.out.println("Elapsed Time in milli seconds: " + (end2 - start2));
+
+	}
+
+	public void combine() {
+		combine(0);
+	}
+
+	private void combine(int start) {
+		for (int i = start; i < inputstring.length(); ++i) {
+			output.append(inputstring.charAt(i));
+			System.out.println(output);
+			if (i < inputstring.length())
+				combine(i + 1);
+			output.setLength(output.length() - 1);
+		}
+	}
+
+}
